44bce93994a4bb678faffe4b59366a2cfc0410b2
Vaidushya <anugya2014@gmail.com>
2025-05-11 14:13:12 +0530

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,28 @@
+import turtle
+turtle.Screen().bgcolor("Aqua")
+
+# first triangle
+turtle.forward(100)
+
+turtle.left(120)
+turtle.forward(100)
+
+turtle.left(120)
+turtle.forward(100)
+
+turtle.penup()
+turtle.right(150)
+turtle.forward(50)
+
+# second tiangle
+turtle.pendown()
+turtle.right(90)
+turtle.forward(100)
+
+turtle.right(120)
+turtle.forward(100)
+
+turtle.right(120)
+turtle.forward(100)
+
+turtle.done
